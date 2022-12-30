7c4f59a6e42e9144e0d52173ce2ecffc548310fd
praveen kumar <praveenshakya1@gmail.com>
2022-12-30 20:33:21 +0530

    initial index.html

---
+++ script.js
@@ -0,0 +1,26 @@
+const dateofBirth = document.querySelector("#date0fBirth");
+const luckyNumber = document.querySelector("#luckyNumber");
+const checkButton = document.querySelector(".btn");
+const outputDiv = document.querySelector(".output");
+
+checkButton.addEventListener("click", function checkBirthDateIsLucky(){
+   const sum = calculateDate(dateofBirth.value);
+
+   if(sum % luckyNumber.value !== 0){
+    outputDiv.innerText = "NOOOO..!! Its not Lucky..!! Do try it in next Life.!! 😋😛";
+   }
+   else{
+    outputDiv.innerText = "Yeah its you lucky year .... 🥰"
+   }
+})
+
+function calculateDate(date){
+    // replace all function will do the job for us
+    date = date.replaceAll("-","");
+    let sum = 0;
+    for(let i = 0; i < date.length; i++){
+        sum += Number(date.charAt(i));
+    }
+    console.log(sum);
+    return sum;
+}
